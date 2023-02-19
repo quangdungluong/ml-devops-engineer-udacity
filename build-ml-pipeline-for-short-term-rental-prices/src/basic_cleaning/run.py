5134c9affafbd6c75f81825d5246cc0303b2b09b
Luong Quang Dung <luongquangdung00@gmail.com>
2023-02-19 10:52:07 +0700

    fix error basic_cleaning

--- build-ml-pipeline-for-short-term-rental-prices/src/basic_cleaning/run.py
+++ build-ml-pipeline-for-short-term-rental-prices/src/basic_cleaning/run.py
@@ -33,7 +33,10 @@ def go(args):
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
-
+    
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     # Save df to csv
     df.to_csv("clean_sample.csv", index=False)
 
